af172ddfa4fb54b7ee3cda1447bdb667ffaab1d1
aleksasperanskaia <aleksandrasperanskaya@gmail.com>
2025-05-02 09:11:59 +0400

    Update romanceclub.cpp

--- romanceclub.cpp
+++ romanceclub.cpp
@@ -58,7 +58,7 @@ RomanceClub::RomanceClub(QWidget *parent) : QMainWindow(parent)
 void RomanceClub::setupAudio()
 {
     mediaPlayer = new QMediaPlayer(this);
-    mediaPlayer->setMedia(QUrl(":/music/background.mp3"));
+    mediaPlayer->setMedia(QUrl(":/music/music/background.mp3"));
     mediaPlayer->setVolume(50);
 
     // Бесконечное повторение
